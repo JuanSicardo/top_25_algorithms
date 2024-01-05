21ee09477edd98fa2676a4636e205abae092915e
Juan Sicardo <juan-antonio-sico@outlook.com>
2024-01-04 18:54:12 -0600

    Improve readability by supressing the use of var in cases where is difficult to infer the type

--- src/main/java/binary_search/BinarySearch.java
+++ src/main/java/binary_search/BinarySearch.java
@@ -8,8 +8,8 @@ public class BinarySearch {
      * The array should be ordered in non-decreasing order
      */
     public int search(int[] array, int target) {
-        var start = 0; // The index of the first element in the search space
-        var end = array.length - 1; // The index of the last element in the search space
+        int start = 0; // The index of the first element in the search space
+        int end = array.length - 1; // The index of the last element in the search space
 
         /* If the target is greater than the pivot check the half with the bigger numbers,
          * if the target is less than the pivot check the half with the smaller numbers,
@@ -18,7 +18,7 @@ public class BinarySearch {
          * If you exhausted the search space then return -1 to indicate you couldn't find the target.
          */
         while (end >= start) {
-            var pivot = start + (end - start) / 2;
+            int pivot = start + (end - start) / 2;
 
             if (array[pivot] < target)
                 start = pivot + 1;
