ea29d7253d9d9f23cc2d41f53e3d9bda136c1c3f
Juan Sicardo <juan-antonio-sico@outlook.com>
2024-01-08 18:37:11 -0600

    Optimize imports

--- src/test/java/binary_search/BinarySearchTest.java
+++ src/test/java/binary_search/BinarySearchTest.java
@@ -2,7 +2,7 @@ package binary_search;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BinarySearchTest {
 
